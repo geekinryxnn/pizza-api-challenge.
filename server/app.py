721719a68493cd37af31ad9a64ab1f79b9ac1389
geekinryxnn <ryankiratu67@gmail.com>
2025-06-16 21:21:14 +0300

    new code

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from config import Config
+from server.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,8 +11,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # Import models and controllers
-from models import restaurant, pizza, restaurant_pizza
-from controllers import restaurant_controller, pizza_controller, restaurant_pizza_controller
+# from models import restaurant, pizza, restaurant_pizza
+# from controllers import restaurant_controller, pizza_controller, restaurant_pizza_controller
 
 if __name__ == '__main__':
     app.run(debug=True)
